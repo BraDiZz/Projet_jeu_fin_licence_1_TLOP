43038c93a76265ccdb76839761a4bba76eb4dde3
Kouadjo <kouadjognaly@gmail.com>
2021-04-16 18:31:17 +0200

    Je viens de rajouter un commentaire

--- src/main.java
+++ src/main.java
@@ -1,8 +1,13 @@
-
+/**
+* Derniere modif : 16/04
+*
+*/
 public class main{
 	public static void main(String[] args) {
 		// Test créé par Kouadjo
 		int age = 0;
 		System.out.println("Tu as : "+age);
+
+
 	}
 }
